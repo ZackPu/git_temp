f76c93f5f69022042bf67863dff98614dfde1819
Zack.Pu <puzhzh@pku.edu.cn>
2021-09-24 01:57:22 +0800

    add demo env

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "Demo_0"
+
+version := "0.1"
+
+scalaVersion := "2.12.13"
+scalacOptions := Seq("-Xsource:2.11")
+
+libraryDependencies += "edu.berkeley.cs" %% "chisel3" % "3.4.4"
+
---
+++ src/main/scala/Demo_0.scala
@@ -0,0 +1,14 @@
+import chisel3._
+import chisel3.Driver
+class Demo_0 extends Module {
+  val io = IO(new Bundle {
+    var in_a = Input(UInt(2.W))
+    var in_b = Input(UInt(1.W))
+    var out = Output(UInt(3.W))
+  })
+  io.out := io.in_a + io.in_b
+}
+
+object Demo_0 extends App {
+  Driver.emitVerilog(new Demo_0())
+}
